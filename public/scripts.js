45906fe3edd8747703f18938492bbcafdabd0029
Andrew Douglass <andrew@wasteddaylight.com>
2014-04-23 00:22:53 -0700

    GeoJSON route working.

--- public/scripts.js
+++ public/scripts.js
@@ -19,10 +19,6 @@ $(document).ready(function(){
         $.getJSON('/locate', {lat: position.coords.latitude, lon: position.coords.longitude}, function(data) {
           
           if(data.located) {
-            map.setView([data.lat, data.lon], 15);
-            
-            $('#logo').hide();
-            $('#location').hide();
 
             var directions = {
               flat: position.coords.latitude,
@@ -32,7 +28,20 @@ $(document).ready(function(){
             };
 
             $.getJSON('/route', directions, function(route) {
-              console.log(route);
+              
+              var geoJsonStyle = {
+                color: "#00f",
+                weight: 10,
+                lineCap: "round",
+                lineJoin: "round"
+              };
+
+              var mappedRoute = L.geoJson(route, {style: geoJsonStyle}).addTo(map);
+
+              $('#logo').hide();
+              $('#location').hide();
+              map.fitBounds(mappedRoute.getBounds());
+              
             });
           }
 
